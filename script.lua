e263125a9e44c02d9cc951c0a506eecf510e5562
quangcaomanhtruong111990-blip <quangcaomanhtruong111990@gmail.com>
2026-02-07 21:13:15 -0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,21 +1,9 @@
--- Đợi game load
-if not game:IsLoaded() then
-    game.Loaded:Wait()
-end
+pcall(function()
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "SCRIPT TEST",
+        Text = "SCRIPT ĐÃ CHẠY",
+        Duration = 5
+    })
+end)
 
-local Players = game:GetService("Players")
-local player = Players.LocalPlayer
-
--- Đợi player tồn tại
-while not player do
-    task.wait()
-    player = Players.LocalPlayer
-end
-
--- Đợi character spawn (sau khi chọn phe)
-player.CharacterAdded:Wait()
-local char = player.Character
-
-print("SCRIPT OK - CHARACTER SPAWNED")
-print("Player:", player.Name)
-print("Character:", char.Name)
+print("SCRIPT EXECUTED")
