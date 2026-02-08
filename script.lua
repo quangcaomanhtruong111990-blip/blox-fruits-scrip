b7c6b7f9606b900645b48d88417d15b8b5ac9ea4
quangcaomanhtruong111990-blip <quangcaomanhtruong111990@gmail.com>
2026-02-07 21:02:20 -0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,12 @@
+print("SCRIPT LOADED OK")
+
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+if player then
+    print("Player Name:", player.Name)
+    print("UserId:", player.UserId)
+else
+    warn("No LocalPlayer")
+end
 
