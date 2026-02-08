cbb4c789951749296d996025463399fe33894426
quangcaomanhtruong111990-blip <quangcaomanhtruong111990@gmail.com>
2026-02-07 21:06:28 -0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,12 +1,17 @@
-print("SCRIPT LOADED OK")
+-- Đợi game load xong
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
 
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 
-if player then
-    print("Player Name:", player.Name)
-    print("UserId:", player.UserId)
-else
-    warn("No LocalPlayer")
+-- Đợi LocalPlayer tồn tại
+while not player do
+    task.wait()
+    player = Players.LocalPlayer
 end
 
+print("SCRIPT LOADED OK")
+print("Player Name:", player.Name)
+print("UserId:", player.UserId)
