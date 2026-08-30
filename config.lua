4c19c7525c845d7bb97ae66e957bd653e21474aa
quangcaomanhtruong111990-blip <quangcaomanhtruong111990@gmail.com>
2026-08-30 17:15:09 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,1 +1,29 @@
+local Config = {
+    Team = "Pirates",
+    Configuration = {
+        HideallPath = false,
+        blackscreen = false,
+        HideGui = false,
+        HopWhenIdle = true,
+        AutoHop = true,
+        AutoHopDelay = 60 * 60,
+        FpsBoost = true,
+        ["IdleCheck"] = 150,
+    },
+    Items = {
+        AutoFullyMelees = true,
+        Saber = true,
+        CursedDualKatana = false,
+        SoulGuitar = false,
+        RaceV2 = false,
+        AutoFarmFruitMastery = false,
+        AutoEatFruit = 1,
+        Eatlist = {}
+    },
+    Settings = {
+        StayInSea2UntilHaveDarkFragments = false,
+        ["Fragments"] = 5000
+    }
+}
 
+return Config
